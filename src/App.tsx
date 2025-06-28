fc47ead4b3f4eeffbcfc3c0dfb8efc6c6f1b241a
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2025-06-28 05:34:51 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+       <div>
+      <h1 style={{ fontFamily: 'Chakra Petch' }}>Texto con Chakra Petch</h1>
+      <p style={{ fontFamily: 'Chakra Petch' }}>Texto con Poppins</p>
+      <p style={{ fontFamily: 'Chakra Petch' }}>Texto con Arial</p>
+    </div>
+    
+  )
+}
+
+export default App
